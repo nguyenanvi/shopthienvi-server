05b11edc1f2e9565d01ddfb183a33249774f5348
Nguyen An Vi <nguyenanvi122333@gmail.com>
2025-05-29 13:02:33 +0700

    PORT copatitive with Heroku issue

--- src/app.js
+++ src/app.js
@@ -309,7 +309,7 @@ app.get("/order/get", isAuthenticated, async (req, res) => {
 
 // ----------------------------------------------
 // Start the Server
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log("environment variables loaded:", process.env.NODE_ENV);
